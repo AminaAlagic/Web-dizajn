77d223d32d175d83606f7de1b10d53d313d4715a
AminaAlagic <104384485+AminaAlagic@users.noreply.github.com>
2022-05-20 21:48:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -104,5 +104,18 @@ const filEditData = (foodId) => {
     }); 
 }
 
+fetch(`${BASE_URL}/api/Food/2`, {
+    method: 'DELETE', 
+    headers: {'content-type': 'application/json'},
+
+})
+  .then(res =>{
+      if(res.ok){
+          console.log("DELETE request successful")
+      }
+      else {console.log("DELETE request unsuccessful")}
+      return res
+  })
+   .then(res => console.log(res))
 
   
